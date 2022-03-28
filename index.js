fa86debb0736c489ef0855060f3949e1ee869c4d
Anya <80169113+me1n0@users.noreply.github.com>
2022-03-28 16:34:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 
-/* tweet count */
+// tweet count
 var tweet = prompt("write");
 var tweetCount = tweet.length;
 alert("you have written " + tweetCount + " characters and have " + (140-tweetCount) + " left" );
+
+// always count from 0
+.slice();
+.length;
+
+//character limit
+var tweet = prompt("write");
+var tweetLimit = tweet.slice(0,3);
+alert(tweetLimit);
+
+// shorter version
+alert(prompt("write").slice(0,3));
