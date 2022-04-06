6a96567114b8222ead4dfbf18bba5d399b9e98ba
Anya <80169113+me1n0@users.noreply.github.com>
2022-04-06 18:01:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -71,3 +71,15 @@ function getMilk(money) {
 }
 
 getMilk(5);
+
+
+// Life in weeks
+function lifeInWeeks(age) {
+    var yearsRemaining = 90 - age;
+    var days = yearsRemaining * 365;
+    var weeks = yearsRemaining * 52;
+    var months = yearsRemaining * 12;
+    prompt("You have " + days + " days, " + weeks + " weeks and " + months + " months left");
+}
+
+lifeInWeeks();
