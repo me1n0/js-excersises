d66d636c334fbdd9bac5616fa72935a6197e233c
Anya <80169113+me1n0@users.noreply.github.com>
2022-04-15 18:43:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -209,15 +209,21 @@ console.log(guessList[0]); //Angela
 // fizzbuzz excersise
 var output = [];
 var count = 1;
-
 function fizzBuzz() {
-    
+    if (count % 3 === 0 && count % 5 === 0){
+    output.push("fizzbuzz");
+    } else if (count % 3 === 0) {
+    output.push("fizz");
+} else if (count % 5 === 0) {
+    output.push("buzz");
+} else {
     output.push(count);
-    count++;
+}
+    
+count++;
     
     console.log(output);
 }
-
 //var output = []; 
 //output.push(1);
 //output.pop;
